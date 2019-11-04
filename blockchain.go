07f93a5c0901208f5414bda63dc4c4b0ea7466a6
pywizard@163.com <you@example.com>
2019-11-04 19:43:08 +0800

    fix 2

--- blockchain.go
+++ blockchain.go
@@ -17,7 +17,7 @@ func NewBlockchain() *BlockChain {
 
 
 // 为区块链  增加一个区块 
-func (bc *Blockchain) AddBlock(data string) {
+func (bc *BlockChain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks) - 1] // 最新一个区块 的Hash 作为 当前区块父Hash
 	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.blocks = append(bc.blocks, newBlock)   // 原来基础上 再增加一个区块
