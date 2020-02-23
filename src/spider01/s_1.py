02164682eadd8e6b527c7b8cbaaad46eb70834ac
pywizard@163.com <you@example.com>
2020-02-23 13:01:51 +0800

    spider01 to db

--- src/spider01/s_1.py
+++ src/spider01/s_1.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import csv
-
+import pymysql
 
 
 
@@ -72,7 +72,7 @@ def getUsefulInfo(items):
 
 
 # 数据存储
-def saveData(result):
+def saveDataCsv(result):
 
 	csvFile = open("../../data/spider01.csv", 'w', newline="", encoding='utf-8')
 	try:
@@ -84,15 +84,46 @@ def saveData(result):
 		csvFile.close()
 
 
+def saveDataMysql(result, fromUrl):
+
+	conn = pymysql.connect(host='', user='', passwd='', db='', charset='utf8')
+	cur = conn.cursor()
+	insertSQL = "insert into searcher_websites (id, webUrl,webName,webDoc,webLogo,webType,cent, fromUrl) value(REPLACE(UUID(),'-',''),'{0}','{1}','{2}','','',0,'{3}')"
+	try:
+		# 写入数据
+		for one in result:
+			tempSql = insertSQL.format(one["url"], one["name"], one["info"], fromUrl)
+			try:
+				cur.execute(tempSql)
+				print("写入成功：" + str(one))
+			except Exception as e:
+				print("写入失败：" + str(one))
+		# 提交
+		conn.commit()
+	except Exception as e:
+		# 错误回滚
+		print(e)
+		conn.rollback()
+	cur.execute("SELECT count(*) FROM searcher_websites")
+	print("总计网站： {} 个".format(cur.fetchone()[0]))
+	cur.close()
+	conn.close()
 
 
 def run():
 	url = 'http://web.naspro.cc/'
 	title = getTitle(url)
+	print("开始采集网址： {}".format(title))
 	items = getItems(url)
 	result = getUsefulInfo(items)
-	saveData(result)
+	saveDataCsv(result)
+	saveDataMysql(result, url)
 	print("共采集网址 {} 个".format(len(items)))
+
+
+
+
 
 if __name__ == "__main__":
 	run()
+
